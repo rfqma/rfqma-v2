b17cad51bccc09a7ac7635c236afdf5d2b149eae
Rifqi Maulana <abelcaster025@gmail.com>
2023-09-14 14:40:01 +0700

    add: mobile layout for home page tech

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -7,7 +7,8 @@ import {
     Link,
     Image,
     useColorModeValue,
-    useColorMode
+    useColorMode,
+    useMediaQuery,
 } from '@chakra-ui/react'
 import { TECHNOLOGIES } from "@/lib/datas"
 import { StaticImageData } from 'next/image';
@@ -16,6 +17,7 @@ export default function Tech() {
     const bg = useColorModeValue('white', 'gray.950')
     const textColor = useColorModeValue('gray.600', 'gray.300')
     const { colorMode } = useColorMode()
+    const [isLargerThan768] = useMediaQuery('(min-width: 768px)')
 
     // Define a function to convert StaticImageData to data URL
     const staticImageToDataURL = (image: StaticImageData): string => {
@@ -23,80 +25,163 @@ export default function Tech() {
         return image.src || ''
     }
 
+    const desktopLayout = (
+        <Container
+            bg={bg}
+            w={'100%'}
+            maxW={'unset'}
+        >
+            <Container
+                maxW={1200}
+                pt={'40'}
+            >
+                <Flex
+                    display={'flex'}
+                    flexDirection={'column'}
+                    alignItems={'center'}
+                    gap={4}
+                >
+                    <Text
+                        fontWeight={'normal'}
+                        fontSize={'1.1rem'}
+                        color={textColor}
+                    >
+                        ⛏️ The tools and technologies I usually use:
+                    </Text>
+                </Flex>
+                <Grid
+                    templateColumns={'repeat(5, minmax(0, 1fr))'}
+                    rowGap={12}
+                    mt={10}
+                >
+                    {
+                        TECHNOLOGIES.map((tech, index) => {
+                            const logoCondition =
+                                colorMode === 'light' ? tech.logo : tech.darkModeLogo
+                            const techLogo = logoCondition
+
+                            return (
+                                <GridItem
+                                    key={index}
+                                    display={'flex'}
+                                    flexDirection={'column'}
+                                    alignItems={'center'}
+                                    gap={2}
+                                >
+                                    <Link href={tech.url}>
+                                        <Image
+                                            src={
+                                                typeof techLogo === 'string'
+                                                    ? techLogo
+                                                    : staticImageToDataURL(techLogo as StaticImageData)
+                                            }
+                                            alt={tech.label}
+                                            _hover={{
+                                                transform: 'scale(1.1)'
+                                            }}
+                                            transition={'transform 0.3s ease'}
+                                            width={65}
+                                            height={65}
+                                        />
+                                    </Link>
+                                    <Text
+                                        fontWeight={'normal'}
+                                        fontSize={'0.8rem'}
+                                        color={textColor}
+                                    >
+                                        {tech.label}
+                                    </Text>
+                                </GridItem>
+                            )
+                        })
+                    }
+                </Grid>
+            </Container>
+        </Container >
+    )
+
+    const mobileLayout = (
+        <Container
+            bg={bg}
+            w={'100%'}
+            maxW={'unset'}
+        >
+            <Container
+                maxW={1200}
+                pt={'40'}
+                textAlign={'center'}
+                alignItems={'center'}
+            >
+                <Flex
+                    display={'flex'}
+                    flexDirection={'column'}
+                    gap={4}
+                >
+                    <Text
+                        fontWeight={'normal'}
+                        fontSize={'1rem'}
+                        color={textColor}
+                    >
+                        🔨 The tools and technologies I usually use:
+                    </Text>
+                </Flex>
+                <Grid
+                    templateColumns={'repeat(3, minmax(0, 1fr))'}
+                    rowGap={8}
+                    mt={10}
+                >
+                    {
+                        TECHNOLOGIES.map((tech, index) => {
+                            const logoCondition =
+                                colorMode === 'light' ? tech.logo : tech.darkModeLogo
+                            const techLogo = logoCondition
+
+                            return (
+                                <GridItem
+                                    key={index}
+                                    display={'flex'}
+                                    flexDirection={'column'}
+                                    alignItems={'center'}
+                                    textAlign={'center'}
+                                    gap={2}
+                                >
+                                    <Link href={tech.url}>
+                                        <Image
+                                            src={
+                                                typeof techLogo === 'string'
+                                                    ? techLogo
+                                                    : staticImageToDataURL(techLogo as StaticImageData)
+                                            }
+                                            alt={tech.label}
+                                            _hover={{
+                                                transform: 'scale(1.1)'
+                                            }}
+                                            transition={'transform 0.3s ease'}
+                                            width={50}
+                                            height={50}
+                                        />
+                                    </Link>
+                                    <Text
+                                        fontWeight={'normal'}
+                                        fontSize={'0.7rem'}
+                                        color={textColor}
+                                    >
+                                        {tech.label}
+                                    </Text>
+                                </GridItem>
+                            )
+                        })
+                    }
+                </Grid>
+            </Container>
+        </Container >
+    )
+
     return (
         <>
-            <Container
-                bg={bg}
-                w={'100%'}
-                maxW={'unset'}
-            >
-                <Container
-                    maxW={1200}
-                    pt={'40'}
-                >
-                    <Flex
-                        display={'flex'}
-                        flexDirection={'column'}
-                        alignItems={'center'}
-                        gap={4}
-                    >
-                        <Text
-                            fontWeight={'normal'}
-                            fontSize={'1.1rem'}
-                            color={textColor}
-                        >
-                            ⛏️ The tools and technologies I usually use:
-                        </Text>
-                    </Flex>
-                    <Grid
-                        templateColumns={'repeat(5, minmax(0, 1fr))'}
-                        rowGap={12}
-                        mt={10}
-                    >
-                        {
-                            TECHNOLOGIES.map((tech, index) => {
-                                const logoCondition =
-                                    colorMode === 'light' ? tech.logo : tech.darkModeLogo
-                                const techLogo = logoCondition
-
-                                return (
-                                    <GridItem
-                                        key={index}
-                                        display={'flex'}
-                                        flexDirection={'column'}
-                                        alignItems={'center'}
-                                        gap={2}
-                                    >
-                                        <Link href={tech.url}>
-                                            <Image
-                                                src={
-                                                    typeof techLogo === 'string'
-                                                        ? techLogo
-                                                        : staticImageToDataURL(techLogo as StaticImageData)
-                                                }
-                                                alt={tech.label}
-                                                _hover={{
-                                                    transform: 'scale(1.1)'
-                                                }}
-                                                transition={'transform 0.3s ease'}
-                                                width={65}
-                                                height={65}
-                                            />
-                                        </Link>
-                                        <Text
-                                            fontWeight={'normal'}
-                                            fontSize={'0.8rem'}
-                                            color={textColor}
-                                        >
-                                            {tech.label}
-                                        </Text>
-                                    </GridItem>
-                                )
-                            })
-                        }
-                    </Grid>
-                </Container>
-            </Container >
+            {
+                isLargerThan768 ? desktopLayout : mobileLayout
+            }
         </>
     )
 }
